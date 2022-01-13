2d8199ebdf10998ca0d97c52e3f34d1ca288591d
Wojtek Skrobot <97315300+skrobot-wojtek@users.noreply.github.com>
2022-01-13 19:13:15 +0100

    Update style.js

--- style.js
+++ style.js
@@ -1,24 +1,35 @@
+// Zadanie 1 
 function sprawdz(){
     let liczba = document.getElementById('pole').value
     console.log(liczba);
-    if (liczba<0){document.getElementById('wynik').innerHTML = "ujemna"}
-    else if (liczba==0){document.getElementById('wynik').innerHTML="zero"}
-    else if (liczba>0){document.getElementById('wynik').innerHTML="dodatnia"}
+    if (liczba<0){document.getElementById('wynik').innerHTML = "UJEMNA"}
+    else if (liczba==0){document.getElementById('wynik').innerHTML="ZERO"}
+    else if (liczba>0){document.getElementById('wynik').innerHTML="DODATNIA"}
 };
-
+// Zadanie 2
 function wypisz(){
     let polea = document.getElementById('polea').value;
     let poleb = document.getElementById('poleb').value;
     let ciag = '';
     if (polea<poleb){
-        for (i=polea; poleb>=i; i++)
+        for (i=polea; poleb>i; i++)
         {
            ciag = ciag +i+" "; 
+
         }document.getElementById('wynik2').innerHTML = ciag; 
     }else {document.getElementById('wynik2').innerHTML = "Pierwsza liczba musi być mniejsza od drugiej!!!"};
 };
-
-function randomer(){
-   let number = Math.floor(Math.random()*5)+1;
+// Zadanie 3
+function randomer1(){
+    let number = Math.floor(Math.random()*50)+1;
     console.log(number);
+    document.getElementById('X').innerHTML="--X--";
+};
+
+function randomer2(){
+    console.log(number);
+    let los = document.getElementById('liczbaWprowadzana').value;
+        if(number<los){document.getElementById('wynik3').innerHTML = "Za dużo spróbój jeszcze raz";}
+        else if(number>los){document.getElementById('wynik3').innerHTML = "Za mało spróbój jeszcze raz";}
+        else if(number=los){document.getElementById('wynik3').innerHTML = "Gratulacje!!! Wygrałeś !!!";}
 };
