c824ff3a9c36b7510bb5d46772026549e2dc516b
Wojtek Skrobot <97315300+skrobot-wojtek@users.noreply.github.com>
2022-01-13 16:29:11 +0100

    Update style.js

--- style.js
+++ style.js
@@ -1,32 +1,24 @@
-// zadanie 1
 function sprawdz(){
+    let liczba = document.getElementById('pole').value
+    console.log(liczba);
+    if (liczba<0){document.getElementById('wynik').innerHTML = "ujemna"}
+    else if (liczba==0){document.getElementById('wynik').innerHTML="zero"}
+    else if (liczba>0){document.getElementById('wynik').innerHTML="dodatnia"}
+};
 
-    let liczba  = document.getElementById('pole').value;
-    if (liczba<0) document.getElementById('wynik').innerHTML='ujemna';
-    else if(liczba==0) document.getElementById('wynik').innerHTML='zero';
-    else if(liczba>0) document.getElementById('wynik').innerHTML='dodatnia';
-    else document.getElementById('wynik').innerHTML='to nie jest lczba';
-}
-// zadanie 2 
 function wypisz(){
+    let polea = document.getElementById('polea').value;
+    let poleb = document.getElementById('poleb').value;
+    let ciag = '';
+    if (polea<poleb){
+        for (i=polea; poleb>=i; i++)
+        {
+           ciag = ciag +i+" "; 
+        }document.getElementById('wynik2').innerHTML = ciag; 
+    }else {document.getElementById('wynik2').innerHTML = "Pierwsza liczba musi być mniejsza od drugiej!!!"};
+};
 
-    let liczba1 = document.getElementById("pole1").value;
-    let liczba2 = document.getElementById("pole2").value;
-    let ciag = "";
-    for(i=liczba1; i<=liczba2; i++)
-    {
-        ciag = ciag+i+' ';
-    }
-    document.getElementById('wynik2').innerHTML=ciag;
-}
-    // zadanie 3 
-    let numer = Math.floor(Math.random()*5)+1;
-    console.log(numer);
-  
-
-    function slajder(){
-       
-  document.getElementById('qwe').innerHTML=numer;
-
-
-    }
+function randomer(){
+   let number = Math.floor(Math.random()*5)+1;
+    console.log(number);
+};
